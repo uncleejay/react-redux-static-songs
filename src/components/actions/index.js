bb843fc92b0bb8acd92f0ca09c92ac551676b27a
uncleejay <joseph.c.o216@gmail.com>
2021-06-19 21:28:06 +0100

    created action and reducer

---
+++ src/components/actions/index.js
@@ -0,0 +1,9 @@
+//actioncreator
+export const selectSong = (song) => {
+    //Return an action
+    return {
+        type: 'SONG_SELECTED',
+        payload: song
+    };
+};
+
---
+++ src/components/reducers/index.js
@@ -0,0 +1,18 @@
+// song list reducer
+const songsReducer = () => {
+    return [
+        {title: 'No Scrubs', duration: '4:05'},
+        {title: 'Macarena', duration:'2:30'},
+        {title: 'All Star', duration:'2:15'},
+        {title: 'I want it that way', duration:'1:45'}
+    ];
+};
+
+
+// selecting a list of song reducer or selected song reducer
+const selectedSongReducer = (selectedSong = null, action) => {
+    if(action.type === 'SONG_SELECTED'){
+        return action.payload;
+    }
+    return selectedSongReducer;
+};
