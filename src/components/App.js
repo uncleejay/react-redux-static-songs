c43c730bb0e1c3a6ad195dd6bfcf39aa905da319
uncleejay <joseph.c.o216@gmail.com>
2021-06-20 22:04:58 +0100

    finished work

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
-    return <div>App</div>;
+  return (
+    <div>
+      <br/>
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
+    </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/actions/index.js
+++
@@ -1,9 +0,0 @@
-//actioncreator
-export const selectSong = (song) => {
-    //Return an action
-    return {
-        type: 'SONG_SELECTED',
-        payload: song
-    };
-};
-
